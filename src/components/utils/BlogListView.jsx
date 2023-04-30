2fdf4fa425f66763d468ba27a645a05484c34029
layrayson <olalereazeez11@gmail.com>
2023-04-30 23:59:42 +0100

    FEAT: create blog preview component

--- src/components/utils/BlogListView.jsx
+++ src/components/utils/BlogListView.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import styles from '../../styles/BlogListView.module.css'
 import BlogPreview from "./BlogPreview";
 
 const BlogsListView = ({blogs}) => {
-    return (<div>
-        {blogs.map((el,index)=><Link to={"/blogs/"+el.id} key={'blog-view-'+index}>
-            <BlogPreview  name={el.title} author={el.author} content={el.content}/>
+    return (<div className={styles.container+" container container-center px-0"}>
+        <div className="row gy-4">
+
+        {blogs.map((el,index)=><Link className="col-4" to={"/blogs/"+el.id} key={'blog-view-'+index}>
+            
+            <BlogPreview  title={el.title} avatar={el.avatar} content={el.content}/>
         </Link>
         )}
+        </div>
     </div>  );
 }
  
--- src/components/utils/BlogPreview.jsx
+++ src/components/utils/BlogPreview.jsx
@@ -1,14 +1,32 @@
-const BlogPreview = ({author,title,content}) => {
-    return ( <div>
-        <div>
-            {title}
+import styles from '../../styles/BlogPreview.module.css'
+const BlogPreview = ({avatar,title,content}) => {
+return(
+    <div className={styles.container+"d-flex flex-column justify-content-start"}>
+        <div className={styles.blogPreviewImage} style={{backgroundImage:`url("${avatar}")`}}>
         </div>
-        <div>
-            {author}
-        </div> <div>
+        <div className={styles.blogTitle}>
+            {title}
+            </div>
+        <div className={styles.blogContent}>
             {content}
         </div>
-    </div> );
+        {/* <div className={styles.blogFooter+' d-flex'}>
+        <div className={styles.authorAvatar}>
+        <img src="/logo512.png" alt="" />
+        </div>
+        <div>
+            <div className={styles.authorName}>s</div>
+            <div className={styles.dateAdded}>
+                <span>Oct 19, 2020</span> . <span>5 min read</span>
+            </div>
+
+        </div>
+        </div> */}
+
+    </div>
+)
+
+  
 }
  
 export default BlogPreview;
